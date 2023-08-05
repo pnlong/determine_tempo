e266171adb52e1a5061c4c922dedcddc2e4c5008
Phillip Long <phillipnlong@gmail.com>
2023-08-04 22:35:24 -0700

    finished train.sh

--- train.sh
+++ train.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-#SBATCH -p free-gpu                     ## run on the gpu partition
-#SBATCH -A tdlong_lab_gpu               ## account to charge
-#SBATCH -t 2:00                         ## 2-minute run time limit
 #SBATCH --job-name=train_tempo_nn       ## pytorch
-#SBATCH -N 1                            ## run on a single node
-#SBATCH -n 1                            ## request 1 task
+#SBATCH -A tdlong_lab_gpu               ## account to charge
+#SBATCH -p free-gpu                     ## run on the gpu partition
+#SBATCH --nodes=1                       ## run on a single node
+#SBATCH --ntasks=1                      ## request 1 task
+#SBATCH --cpus-per-task=1               ## number of cores the job needs
+#SBATCH -t 2:00                         ## 2-minute run time limit
 #SBATCH --gres=gpu:V100:1               ## request 1 gpu of type V100
+#SBATCH --error=/dfs7/adl/pnlong/artificial_dj/determine_tempo/train_tempo_nn.err            ## error log file
+#SBATCH --output=/dfs7/adl/pnlong/artificial_dj/determine_tempo/train_tempo_nn.out           ## output log file
 
 # train the neural network on a cluster; request GPU partition
 
