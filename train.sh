cc413733350682bb813968b601e9e6e136e40cec
Phillip Long <phillipnlong@gmail.com>
2023-08-13 19:46:33 -0700

    fixed device registration error

--- train.sh
+++ train.sh
@@ -6,7 +6,6 @@
 #SBATCH --ntasks=1                      ## request 1 task
 #SBATCH --cpus-per-task=1               ## number of cores the job needs
 #SBATCH --gres=gpu:V100:1               ## request 1 gpu of type V100
-#SBATCH --output=/dfs7/adl/pnlong/artificial_dj/determine_tempo/train.out           ## output log file
 
 # README
 # Phillip Long
