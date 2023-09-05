0bf9296b6d441129a91aa035b52cef0449d1ec80
Phillip Long <phillipnlong@gmail.com>
2023-09-05 00:23:02 -0700

    added determine_tempo

---
+++ determine_tempo.py
@@ -0,0 +1,16 @@
+# README
+# Phillip Long
+# September 5, 2023
+
+# Given a song, output its predicted tempo (in BPM).
+
+# python ./determine_tempo.py nn_filepath song_filepath
+
+
+# IMPORTS
+##################################################
+import sys
+# sys.argv = ("./determine_tempo.py", "/Users/philliplong/Desktop/Coding/artificial_dj/data/tempo_nn.pth", "")
+##################################################
+
+
